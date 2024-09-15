02494d04b4e6a810aa7ebdfe08bea2aa57da9085
arletino <titkov@nxt.ru>
2024-09-15 22:59:55 +0300

    pydantic first step

--- Pydantic_v2.py
+++ Pydantic_v2.py
@@ -6,15 +6,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict, EnvSettingsSourc
 
 os.environ['PRODUCTION_AUTH_KEY'] = 'test_auth_key'
 os.environ["PRODUCTION_MY_API_KEY"] = 'test'
-os.environ['PRODUCTION_ENV2'] = 'https://mysuperurl.ru'
-#print(os.environ)
+os.environ['PRODUCTION_ENV1'] = 'https://mysuperurl.ru'
+print(os.environ)
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_prefix='PRODUCTION_', env_file='.env')
 
     service_name: str = Field(default='default')
     auth_key: str 
-    key: str = Field(alias='my_api_key')
+    my_api_key: str = Field() # alias='my_key')
+    # my_api_key: str 
     url: str = Field(validation_alias=AliasChoices('env1', 'env2'))
+    
+    model_config = SettingsConfigDict()
+    # model_config['env_file'] = '.env'
+    model_config['env_prefix'] = 'production_' 
+    print(model_config)
+    # model_config = SettingsConfigDict(env_prefix='production_', env_file='.env', case_sensitive=False)
 
 print(Settings().model_dump())
